120c84ed18fdeaf5ade75daee26cb36f33a90672
TasukuY <alohaty81@gmail.com>
2022-07-24 20:36:28 -0600

    Initial Commit

---
+++ AppleID_Login_Test/AppleID_Login_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleID_Login_TestApp.swift
+//  AppleID_Login_Test
+//
+//  Created by Tasuku Yamamoto on 7/24/22.
+//
+
+import SwiftUI
+
+@main
+struct AppleID_Login_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
